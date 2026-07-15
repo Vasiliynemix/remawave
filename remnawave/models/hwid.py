411e926af301e9cd1df1033e0868fdd879400687
Vasiliynemix <nemixvasya@gmail.com>
2026-07-15 09:37:47 +0200

    add platform to HwidDeviceDto

--- remnawave/models/hwid.py
+++ remnawave/models/hwid.py
@@ -24,6 +24,7 @@ class HwidDeviceDto(BaseModel):
     hwid: str
     user_uuid: Optional[UUID] = Field(default=None, alias="userUuid")  # Ваша версия кидает ошибку на новом API 2.8.0
     user_id: Optional[int] = Field(default=None, alias="userId")  # Добавил для исключения ошибок указанных снизу
+    platform: Optional[str] = None
     os_version: Optional[str] = Field(None, alias="osVersion")
     device_model: Optional[str] = Field(None, alias="deviceModel")
     user_agent: Optional[str] = Field(None, alias="userAgent")
